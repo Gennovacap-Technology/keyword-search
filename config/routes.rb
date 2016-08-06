c3b455e386406a28f3d824531134df7f501b797f
arup rakshit <aruprakshit@rocketmail.com>
2016-08-06 16:18:43 +0530

    Add Keywords controller index view and search form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  controller :keywords, as: 'keywords' do
+    get 'index'
+    get 'search'
+  end
+
+  root to: "keywords#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
